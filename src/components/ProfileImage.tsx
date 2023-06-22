7b92d73c82700ea7c336bc24e45cfcf1f638208f
steven <alvaradosteven315@gmail.com>
2023-06-22 12:11:26 -0500

    style sidebar with icons

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import type { FC } from "react";
+import { VscAccount } from "react-icons/vsc";
 
 type ProfileImageProps = {
   src?: string | null;
@@ -11,7 +12,9 @@ const ProfileImage: FC<ProfileImageProps> = ({ src, className = "" }) => {
     <div
       className={`relative aspect-square w-12 overflow-hidden rounded-full ${className}`}
     >
-      {src == null ? null : (
+      {src == null ? (
+        <VscAccount className="h-full w-full" />
+      ) : (
         <Image src={src} alt="profile image" quality={100} fill />
       )}
     </div>
--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,8 +1,8 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import { FC } from "react";
+import type { FC } from "react";
 import IconHoverEffect from "./IconHoverEffect";
-import { VscHome } from "react-icons/vsc";
+import { VscAccount, VscHome, VscSignIn, VscSignOut } from "react-icons/vsc";
 
 const SideNav: FC = ({}) => {
   const session = useSession();
@@ -10,7 +10,7 @@ const SideNav: FC = ({}) => {
   const user = session.data?.user;
 
   return (
-    <nav className="sticky top-0  p-2 ">
+    <nav className="sticky top-0  p-2">
       <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
         <li>
           <Link href="/">
@@ -24,17 +24,42 @@ const SideNav: FC = ({}) => {
         </li>
         {user !== null && user !== undefined && (
           <li>
-            <Link href={`/profile/${user.id}`}>Profile</Link>
+            <Link href={`/profile/${user.id}`}>
+              <IconHoverEffect>
+                <span className="flex items-center gap-4">
+                  <VscAccount className="h-8 w-8" />
+                  <span className="hidden text-lg md:inline">Profile</span>
+                </span>
+              </IconHoverEffect>
+            </Link>
           </li>
         )}
 
         {user == null ? (
           <li>
-            <button onClick={() => void signIn()}>Log In</button>
+            <button onClick={() => void signIn()}>
+              <IconHoverEffect>
+                <span className="flex items-center gap-4">
+                  <VscSignIn className="h-8 w-8 fill-green-500/80" />
+                  <span className="hidden text-lg text-green-500/80 md:inline">
+                    Log In
+                  </span>
+                </span>
+              </IconHoverEffect>
+            </button>
           </li>
         ) : (
           <li>
-            <button onClick={() => void signOut()}>Log Out</button>
+            <button onClick={() => void signOut()}>
+              <IconHoverEffect>
+                <span className="flex items-center gap-4">
+                  <VscSignOut className="h-8 w-8 fill-red-500/80" />
+                  <span className="hidden text-lg text-red-500/80 md:inline">
+                    Log Out
+                  </span>
+                </span>
+              </IconHoverEffect>
+            </button>
           </li>
         )}
       </ul>
